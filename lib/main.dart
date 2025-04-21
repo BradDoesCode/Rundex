4531f8ef561c2820098191a6ca170ed0c459c035
BradDoesCode <bradbarbrook@gmail.com>
2025-04-21 19:49:31 +0100

    provider scope for riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
   Hive.initFlutter();
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp(),));
 }
 
 class MyApp extends StatelessWidget {
