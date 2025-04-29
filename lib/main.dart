0d60eb5bbd709fe70c7db71004313547723c5389
BradDoesCode <bradbarbrook@gmail.com>
2025-04-29 11:31:35 +0100

    main dart tidy up

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:dragonwilds_companion/classes/collectibles/collectibles.dart';
-import 'package:dragonwilds_companion/classes/provider/completed_items.dart';
 import 'package:dragonwilds_companion/classes/quest/quest.dart';
 import 'package:dragonwilds_companion/screens/home_screen.dart';
 import 'package:dragonwilds_companion/theme.dart';
 import 'package:dragonwilds_companion/utils/utils.dart';
 import 'package:dragonwilds_companion/widgets/drawer.dart';
-import 'package:dragonwilds_companion/widgets/progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -81,143 +79,7 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
       drawer: DrawerMenu(),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: RadialGradient(
-            colors: [
-              Color(0xFF342496), // Outer color
-              Color(0xFF34296D), // Inner color
-            ],
-            center: Alignment.center,
-            radius: 1.0,
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              TotalProgress(),
-              Stack(
-                children: [
-                  WelcomeBack(),
-                  Padding(
-                      padding: const EdgeInsets.only(
-                          left: 16.0, right: 16.0, top: 150),
-                      child: HomeScreen()),
-                  OverClaw(),
-                ],
-              ),
-            ],
-          ),
-        ), // Your body content
-      ),
-    );
-  }
-}
-
-class OverClaw extends StatelessWidget {
-  const OverClaw({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      top: 3,
-      right: 0,
-      child: Image.asset(
-        'assets/home_claw.png',
-        width: MediaQuery.of(context).size.width * 0.6,
-        fit: BoxFit.cover,
-      ),
-    );
-  }
-}
-
-class WelcomeBack extends StatelessWidget {
-  const WelcomeBack({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 150,
-      child: Stack(
-        children: [
-          Positioned(
-            bottom: 0,
-            left: 30,
-            child: Text(
-              'WELCOME\nBACK,\nSLAYER!',
-              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: Colors.white,
-                  ),
-            ),
-          ),
-          Positioned(
-              top: 0,
-              right: 0,
-              child: Image.asset(
-                'assets/home_dragon.png',
-                width: MediaQuery.of(context).size.width * 0.6,
-                fit: BoxFit.cover,
-              ))
-        ],
-      ),
-    );
-  }
-}
-
-class TotalProgress extends ConsumerWidget {
-  const TotalProgress({super.key});
-
-  int calculateTotal(Map<String, List<String>> completedItems) {
-    int allItems = kMainQuests.fold<int>(0, (int sum, quest) {
-          return sum + (quest.steps?.length ?? 0);
-        }) +
-        kSideQuests.fold<int>(0, (int sum, quest) {
-          return sum + (quest.steps?.length ?? 0);
-        }) +
-        kLoreScraps.length;
-    int completed = completedItems.values.fold<int>(0, (int sum, list) {
-      return sum + list.length;
-    });
-    return completionPercentage(completed, allItems);
-  }
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final completedItems = ref.watch(completedItemsProvider).value;
-    final percent = calculateTotal(completedItems ?? {});
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0, right: 16, top: 32),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'Your Adventure',
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                fontWeight: FontWeight.w400, fontSize: 20, color: Colors.white),
-          ),
-          const SizedBox(height: 4),
-          ProgressBar(progress: percent),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                '$percent%',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(color: Colors.white),
-              ),
-            ],
-          )
-        ],
-      ),
+      body: HomeScreen(),
     );
   }
 }
